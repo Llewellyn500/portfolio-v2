301f7b2f0056e1f5b1f50a4b6ee6da621e15505c
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2021-03-22 20:00:36 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if (!navigator.serviceWorker.controller) {
-  navigator.serviceWorker.register(".././sw.js").then(function (reg) {
+  navigator.serviceWorker.register("https://raw.githubusercontent.com/Llewellyn500/portfolio/main/sw.js").then(function (reg) {
     console.log("Service worker has been registered for scope: " + reg.scope);
   });
 }
