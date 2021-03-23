3288b555ec6bc37923f5cab74c2c1b13d9545a1c
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2021-03-23 08:51:22 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ var preLoad = function () {
   console.log("Installing web app");
   return caches.open("offline").then(function (cache) {
     console.log("caching index and important routes");
-    return cache.addAll(["./offline.html"]);
+    return cache.addAll(["https://raw.githubusercontent.com/Llewellyn500/portfolio/main/offline.html"]);
   });
 };
 
@@ -44,7 +44,7 @@ var returnFromCache = function (request) {
   return caches.open("offline").then(function (cache) {
     return cache.match(request).then(function (matching) {
       if (!matching || matching.status == 404) {
-        return cache.match("offline.html");
+        return cache.match("https://raw.githubusercontent.com/Llewellyn500/portfolio/main/offline.html");
       } else {
         return matching;
       }
